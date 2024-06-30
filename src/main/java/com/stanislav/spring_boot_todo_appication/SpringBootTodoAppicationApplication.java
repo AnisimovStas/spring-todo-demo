db6234c597ca7b8de1873252c0f37713a9cbbc9f
Stanislav <AnisimovSA@apheco.ru>
2024-06-30 13:43:52 +0400

    init commit

---
+++ src/main/java/com/stanislav/spring_boot_todo_appication/SpringBootTodoAppicationApplication.java
@@ -0,0 +1,13 @@
+package com.stanislav.spring_boot_todo_appication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoAppicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodoAppicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stanislav/spring_boot_todo_appication/SpringBootTodoAppicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanislav.spring_boot_todo_appication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoAppicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
